fb3957d7f9bd1d9ce1d7dd6e90065a8853ab6a70
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2025-10-09 14:02:59 +0500

    updated

---
+++ start-server.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting ProjectPlanner PHP Server...
+echo.
+echo Server will be available at: http://localhost:8000
+echo Press Ctrl+C to stop the server
+echo.
+php -S localhost:8000 router.php
